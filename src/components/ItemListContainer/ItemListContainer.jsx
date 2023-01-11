1eb6b53f51a436d810080358ee390dbcb3ef09ac
Belen Robledo <beeelrobledo@gmail.com>
2023-01-11 19:45:21 -0300

    2da Entrega Proyecto Final

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,28 +3,38 @@ import { useParams } from "react-router-dom";
 
 import getAllProducts, { getProductsByCategory } from '../../services/mockService'
 import Card from "../Card/Card"
+import Loading from "../../pages/Loading";
 
 export default function ItemListContainer(){
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
     let {categoryid} = useParams();
 
     useEffect(() => {
         if(!categoryid){
             getAllProducts().then((response) => {
                 setProducts(response);
-              }).catch((error) => alert(error));
+              }).catch((error) => alert(error)
+              ).finally( () => setLoading(false));
         } else{
             getProductsByCategory(categoryid).then((response) => {
                 setProducts(response);
-              }).catch((error) => alert(error));
+              }).catch((error) => alert(error)
+              ).finally( () => setLoading(false));
         }
     }, [categoryid]);
 
     return (
-        <div>
-            <div className="container">
-              {products.map((product) => <Card key={product.id} product={product}/>)}
+        <>
+        {loading ?
+            <Loading/>
+        :
+            <div>
+                <div className="container">
+                {products.map((product) => <Card key={product.id} product={product}/>)}
+                </div>
             </div>
-        </div>
+        }
+        </>
     );
 }
